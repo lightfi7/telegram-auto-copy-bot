f721d244f0f6cd12072c45a7a454dbdc047ebf5a
Fi <158217226+lightfi7@users.noreply.github.com>
2024-04-05 03:07:56 -0700

    updated

--- main.py
+++ main.py
@@ -1,16 +1,120 @@
-# This is a sample Python script.
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import logging
+from typing import Optional, Tuple
+
+from telegram import Chat, ChatMember, ChatMemberUpdated, Update
+from telegram.constants import ParseMode
+from telegram.ext import (
+    Application,
+    ChatMemberHandler,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+# Replace 'SOURCE_CHANNEL_ID' with the ID of the channel you want to copy messages from
+SOURCE_CHANNEL_ID = '-1002053179330'
+
+# Replace 'DESTINATION_CHAT_ID' with the ID of the user or chat you want to send messages to
+DESTINATION_CHAT_ID = '5422802236'  #'-4189617390'
+
+print(BOT_TOKEN)
+
+# Enable logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+async def copy_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Greets the user and records that they started a chat with the bot if it's a private chat.
+    Since no `my_chat_member` update is issued when a user starts a private chat with the bot
+    for the first time, we have to track it explicitly here.
+    """
+    print(update)
+    logger.info("chat_id = %s", update.channel_post.chat.id)
+    if update.channel_post.chat.id == int(SOURCE_CHANNEL_ID):
+        # Copy the message to the destination chat
+        await context.bot.copy_message(chat_id=DESTINATION_CHAT_ID, from_chat_id=update.channel_post.chat.id,
+                                       message_id=update.channel_post.message_id)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def extract_status_change(chat_member_update: ChatMemberUpdated) -> Optional[Tuple[bool, bool]]:
+    """Takes a ChatMemberUpdated instance and extracts whether the 'old_chat_member' was a member
+    of the chat and whether the 'new_chat_member' is a member of the chat. Returns None, if
+    the status didn't change.
+    """
+    status_change = chat_member_update.difference().get("status")
+    old_is_member, new_is_member = chat_member_update.difference().get("is_member", (None, None))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if status_change is None:
+        return None
+
+    old_status, new_status = status_change
+    was_member = old_status in [
+        ChatMember.MEMBER,
+        ChatMember.OWNER,
+        ChatMember.ADMINISTRATOR,
+    ] or (old_status == ChatMember.RESTRICTED and old_is_member is True)
+    is_member = new_status in [
+        ChatMember.MEMBER,
+        ChatMember.OWNER,
+        ChatMember.ADMINISTRATOR,
+    ] or (new_status == ChatMember.RESTRICTED and new_is_member is True)
+
+    return was_member, is_member
+
+
+async def greet_chat_members(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Greets new users in chats and announces when someone leaves"""
+    result = extract_status_change(update.chat_member)
+    if result is None:
+        return
+
+    was_member, is_member = result
+    cause_name = update.chat_member.from_user.mention_html()
+    member_name = update.chat_member.new_chat_member.user.mention_html()
+
+    if not was_member and is_member:
+        await update.effective_chat.send_message(
+            f"{member_name} was added by {cause_name}. Welcome!",
+            parse_mode=ParseMode.HTML,
+        )
+    elif was_member and not is_member:
+        await update.effective_chat.send_message(
+            f"{member_name} is no longer with us. Thanks a lot, {cause_name} ...",
+            parse_mode=ParseMode.HTML,
+        )
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    # Handle members joining/leaving chats.
+    application.add_handler(ChatMemberHandler(greet_chat_members, ChatMemberHandler.CHAT_MEMBER))
+
+    # Create a message handler that listens for new messages in the source channel
+    message_handler = MessageHandler(filters.ALL, copy_message)
+    application.add_handler(message_handler)
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
